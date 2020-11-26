2bbaffceb4d1f773a689823bf70bb60d8d7e2a6a
shresthapr <shresthaprajan38@gmail.com>
2020-11-26 23:57:45 +0200

    playerlsit displayed, add a single player at a time

---
+++ src/Component/Favourite/favourite.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './favourite.css';
+
+const Favourite = ({ fname, sname, score, cost, form, points, image }) => {
+  return (<div className="fav-list">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Score</th>
+          <th>Cost</th>
+          <th>Form</th>
+          <th>Points</th>
+          <th>Image</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>{fname} {sname}</th>
+          <th>{score}</th>
+          <th>{cost}</th>
+          <th>{form}</th>
+          <th>{points}</th>
+          <th><img src={image} /></th>
+        </tr>
+      </tbody>
+    </table>
+
+  </div >
+
+  );
+};
+
+
+
+export default Favourite;
--- src/Component/Player/Player.js
+++ src/Component/Player/Player.js
@@ -1,46 +1,80 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import './Player.css';
+import Favourite from '../Favourite/favourite'
+  ;
 const API = "https://cors-anywhere.herokuapp.com/https://fantasy.premierleague.com/api/bootstrap-static/";
+const params = {
+  _limit: 1,
+}
+
+
 class Player extends Component {
   state = {
     league: {
       events: [],
-      teams: [],
+      // teams: [],
       elements: [],
-      element_stats: [],
-      element_types: [],
+      // element_stats: [],
+      // element_types: [],
     },
     isLoading: false,
+    fname: '',
+    sname: '',
+    cost: '',
+    score: '',
+    form: '',
+    points: '',
+    image: '',
   }
 
   componentDidMount() {
     this.setState({ isLaoding: true });
-    axios.get(API, {
-      params: {
-        _limit: 1,
-      },
-    })
-      .then((response) => this.setState({ league: response.data, isLoading: false }))
-
+    axios.get(API, { params })
+      .then((response) => this.setState({ league: response.data, isLoading: false }));
   }
-
+  addHandler(id) {
+    const favlist = this.state.league.elements.find((player) => player.id == id);
+    this.setState({
+      fname: favlist.first_name,
+      sname: favlist.second_name,
+      score: favlist.goals_scored,
+      cost: favlist.now_cost,
+      form: favlist.form,
+      points: favlist.total_points,
+      image: favlist.photo,
+    });
+  }
 
   render() {
     if (this.state.isLoading) { return <p>Loading.....</p> }
     return (
       <div>
-        <h2>Player list</h2>
+        <Favourite
+          fname={this.state.fname}
+          sname={this.state.sname}
+          score={this.state.score}
+          cost={this.state.cost}
+          form={this.state.form}
+          points={this.state.points}
+          image={this.state.image} />
 
-        <ul>{this.state.league.events.map((items) =>
-
-          (<li>{items.id}
-            <p>{items.name}</p>
-          </li>)
-
-        )}</ul>
-
-
+        <div>
+          <h2>Player list</h2>
+          <ul className="playerslist">{this.state.league.elements.map((items) =>
+            (<li className="player" key={items.id}>
+              <h2>{items.first_name}</h2>
+              <p>{items.second_name}</p>
+              <p>{items.form}</p>
+              <p>{items.now_cost}</p>
+              <p>{items.goals_scored}</p>
+              <p>{items.total_points}</p>
+              <p>{items.creativity}</p>
+              <img src={items.photo} />
+              <div className="add" onClick={() => this.addHandler(items.id)}>+ </div>
+            </li>)
+          )}</ul>
+        </div>
       </div>
     );
   }
