7ffd5629f55ca9c069f3fe38dfbd4eede47787b1
Juha Kangas <juhakangas5@gmail.com>
2020-11-27 15:09:17 +0200

    Added firebase config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header/Header'
 import Player from './Component/Player/Player';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Welcome to Fantasy Premier League</h1>
       <Player />
     </div>
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { auth } from '../../firebase'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                Header
+            </div>
+        )
+    }
+}
