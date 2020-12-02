50d8cec406860d3dc9735a3017bc1305bafb6b73
Louis Atkinson <atkinsonlouis@gmail.com>
2020-12-02 12:40:12 +0200

    created main component used for routing different pages

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Signup from "../pages/Signup";
+
+const Main = () => {
+  return (
+    <Switch>
+      {" "}
+      {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/signup" component={Player}></Route>
+    </Switch>
+  );
+};
+
+export default Main;
