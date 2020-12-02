885fec0449ecad29425015a3d3c27cf8771a8d2a
Louis Atkinson <atkinsonlouis@gmail.com>
2020-12-02 12:33:23 +0200

    Premier league table is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Player from './Component/Player/Player';
+import logo from "./logo.svg";
+import "./App.css";
+import Player from "./Component/Player/Player";
+import Table from "./Component/table/Table.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Fantasy Premier League</h1>
       <Player />
+      <Table />
     </div>
   );
 }
