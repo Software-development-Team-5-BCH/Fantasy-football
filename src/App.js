6c405ab444c2b73766122bcbf8c38f50ee382f64
shresthapr <shresthaprajan38@gmail.com>
2020-12-10 01:19:14 +0200

    Search Club team roster added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Player from './Component/Player/Player';
+import Club from './Component/Club/Club'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Fantasy Premier League</h1>
+      <Club />
       <Player />
+
     </div>
   );
 }
--- src/Component/Player/Player.js
+++ src/Component/Player/Player.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './Player.css';
-import Favourite from '../Favourite/favourite'
-  ;
-const API = "https://cors-anywhere.herokuapp.com/https://fantasy.premierleague.com/api/bootstrap-static/";
+import React, { Component } from "react";
+import axios from "axios";
+import "./Player.css";
+import Clubs from "../Club/Club"
+import Favourite from "../Favourite/favourite";
+const API = "https://fantasy.premierleague.com/api/bootstrap-static/";
 const params = {
   _limit: 1,
-}
+};
+
 class Player extends Component {
   state = {
     league: {
@@ -17,58 +18,43 @@ class Player extends Component {
       // element_types: [],
     },
     isLoading: false,
-
-    fname: '',
-    sname: '',
-    cost: '',
-    score: '',
-    form: '',
-    points: '',
-    image: '',
-
-  }
+    fname: "",
+    sname: "",
+    cost: "",
+    score: "",
+    form: "",
+    points: "",
+    image: "",
+  };
 
   componentDidMount() {
-
     this.setState({ isLaoding: true });
-    axios.get(API, { params })
-      .then((response) => this.setState({ league: response.data, isLoading: false }));
-    // const data = axios.get(API, { params })
-    //   .then((res => res.json()))
-
-    // console.log(data)
-    // localStorage.setItem("mydata", JSON.stringify(data));
-    // let getData = JSON.parse(localStorage.getItem('mydata'));
-    // this.setState({ league: getData })
-
+    axios
+      .get(API, { params })
+      .then((response) =>
+        this.setState({ league: response.data, isLoading: false })
+      );
   }
-
-
   addHandler(id) {
-    let tablelist = [];
-    const favlist = JSON.stringify(this.state.league.elements.find((player) => player.id == id));
-    console.log(favlist)
-    localStorage.setItem('player', favlist);
-    let detail = JSON.parse(localStorage.getItem("player"))
-    console.log(detail)
-    const data = this.setState({
-      fname: detail.first_name,
-      sname: detail.second_name,
-      score: detail.goals_scored,
-      cost: detail.now_cost,
-      form: detail.form,
-      points: detail.total_points,
-      image: detail.photo,
+    console.log(id)
+    const favlist = this.state.league.elements.find(
+      (player) => player.id == id
+    );
+    this.setState({
+      fname: favlist.first_name,
+      sname: favlist.second_name,
+      score: favlist.goals_scored,
+      cost: favlist.now_cost,
+      form: favlist.form,
+      points: favlist.total_points,
+      image: favlist.photo,
     });
-    tablelist.push(data);
-    console.log(tablelist)
   }
-
-
-
 
   render() {
-    if (this.state.isLoading) { return <p>Loading.....</p> }
+    if (this.state.isLoading) {
+      return <p>Loading.....</p>;
+    }
     return (
       <div>
         <Favourite
@@ -78,28 +64,32 @@ class Player extends Component {
           cost={this.state.cost}
           form={this.state.form}
           points={this.state.points}
-          image={this.state.image} />
-
+          image={this.state.image}
+        />
+        <Clubs include={this.addHandler} />
         <div>
           <h2>Player list</h2>
-          <ul className="playerslist">{this.state.league.elements.map((items) =>
-            (<li className="player" key={items.id}>
-              <h2>{items.first_name}</h2>
-              <p>{items.second_name}</p>
-              <p>{items.form}</p>
-              <p>{items.now_cost}</p>
-              <p>{items.goals_scored}</p>
-              <p>{items.total_points}</p>
-              <p>{items.creativity}</p>
-              <img src={items.photo} alt={items.fname} />
-              <div className="add" onClick={() => this.addHandler(items.id)}>+ </div>
-            </li>)
-          )}</ul>
+          <ul className="playerslist">
+            {this.state.league.elements.map((items) => (
+              <li className="player" key={items.id}>
+                <h2>{items.first_name}</h2>
+                <p>{items.second_name}</p>
+                <p>{items.form}</p>
+                <p>{items.now_cost}</p>
+                <p>{items.goals_scored}</p>
+                <p>{items.total_points}</p>
+                <p>{items.creativity}</p>
+                <img src={items.photo} />
+                <div className="add" onClick={() => this.addHandler(items.id)}>
+                  +{" "}
+                </div>
+              </li>
+            ))}
+          </ul>
         </div>
-      </div>
+      </div >
     );
   }
 }
-
 
 export default Player;
